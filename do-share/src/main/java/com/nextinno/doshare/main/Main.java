5079b4c6e5f8cf7dab54d31f9de61dab8e56ed24
rsjung <rsjung@DESKTOP-CN8U2MC>
2016-09-03 16:16:44 +0900

    cors 처리

--- do-share/src/main/java/com/nextinno/doshare/main/Main.java
+++ do-share/src/main/java/com/nextinno/doshare/main/Main.java
@@ -8,7 +8,10 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -23,18 +26,20 @@ public class Main {
 
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
-    @RequestMapping("/")
+    /**
+     * @return
+     */
+//    @CrossOrigin(origins = "http://localhost:9000")
+    @GetMapping("/login")
     // 나는 json으로만 데이터를 주고 받을 것이니 @ResponseBody는 없도록 한다.
     @ResponseBody
     String home() {
         logger.info("home API Request!");
-        logger.debug("why??????");
-        
-        return "Hello World!";
+        return String.format("hello workd");
     }
-    
+
     public static void main(String[] args) {
-        
+
         SpringApplication.run(Main.class, args);
         logger.info("main start!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         String LogLevel = DoShareConfig.getProperty("LogLevel");
