16bb107a624eb1c83aa87a3840d1a64e879284bf
rsjung <rsjung@DESKTOP-CN8U2MC>
2016-09-07 17:59:35 +0900

    MyBatis 설정 및 테스트 코드 삽입

--- do-share/src/main/java/com/nextinno/doshare/main/Main.java
+++ do-share/src/main/java/com/nextinno/doshare/main/Main.java
@@ -8,11 +8,9 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.nextinno.doshare.config.DoShareConfig;
@@ -25,23 +23,23 @@ import com.nextinno.doshare.config.DoShareConfig;
 public class Main {
 
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
-
+    
     /**
      * @return
      */
-//    @CrossOrigin(origins = "http://localhost:9000")
-    @GetMapping("/login")
+    @GetMapping("/user")
     // 나는 json으로만 데이터를 주고 받을 것이니 @ResponseBody는 없도록 한다.
     @ResponseBody
     String home() {
         logger.info("home API Request!");
-        return String.format("hello workd");
+        return "connect/login";
     }
 
     public static void main(String[] args) {
 
         SpringApplication.run(Main.class, args);
         logger.info("main start!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        
         String LogLevel = DoShareConfig.getProperty("LogLevel");
         logger.info("LogLevel : " + LogLevel);
         logger.debug("main??????");
