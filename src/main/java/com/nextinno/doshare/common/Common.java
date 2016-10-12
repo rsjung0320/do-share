4ed3f43ef4dcab9e9b7b471a42b13370d3f1a1e9
rsjung0320 <rsjung0320@gmail.com>
2016-10-12 17:55:31 +0900

    1. Token 도메인 생성 2. Refresh Token 생성 함수 개발 3. LoginController 리팩토링 4. 로그인 시 remember me의 값으로 true이면 refreah token 발행 하는 것 개발

--- src/main/java/com/nextinno/doshare/common/Common.java
+++ src/main/java/com/nextinno/doshare/common/Common.java
@@ -8,7 +8,6 @@ import java.util.Date;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.nextinno.doshare.config.DoShareConfig;
@@ -20,24 +19,21 @@ import com.nextinno.doshare.config.DoShareConfig;
 @Component
 public class Common {
     private static final Logger logger = LoggerFactory.getLogger(Common.class);
-    
-    public Common(){
+
+    public Common() {
         logger.info("Common Start.!");
     }
-    
+
     public Claims getClaimsFromToken(String token) {
         Claims claims;
         try {
-            claims = Jwts.parser()
-                    .setSigningKey("nextInno")
-                    .parseClaimsJws(token)
-                    .getBody();
+            claims = Jwts.parser().setSigningKey("nextInno").parseClaimsJws(token).getBody();
         } catch (Exception e) {
             claims = null;
         }
         return claims;
     }
-    
+
     public String getUsernameFromToken(String token) {
         String username;
         try {
@@ -48,18 +44,33 @@ public class Common {
         }
         return username;
     }
-    
+
     /**
      * 토큰을 생성하는 function
+     * 
      * @param email
      * @param role
      * @return
      */
     public static String generateToken(String email, String role) {
-        return Jwts.builder().setSubject(email)
-                .claim("roles", role).setIssuedAt(new Date())
+        Date today = new Date();
+        Date tomorrow = new Date(today.getTime() + (1000 * 60 * 60 * 2));
+        return Jwts.builder().claim("roles", role).setIssuedAt(today).setExpiration(tomorrow).setIssuer(email)
+                .signWith(SignatureAlgorithm.HS256, DoShareConfig.SECRET).compact();
+    }
+
+    /**
+     * remember me를 선택한 사람만 주는 토큰(mobile 같은 경우도 아래와 같이 한다.)
+     * 
+     * @param email
+     * @param role
+     * @return
+     */
+    public static String generateRefreshToken(String email, String role) {
+        Date today = new Date();
+        Date oneMonth = new Date(today.getTime() + (1000 * 60 * 60 * 24 * 30));
+
+        return Jwts.builder().claim("roles", role).setIssuedAt(today).setExpiration(oneMonth).setIssuer(email)
                 .signWith(SignatureAlgorithm.HS256, DoShareConfig.SECRET).compact();
     }
 }
-    
-    
--- src/main/java/com/nextinno/doshare/login/LoginController.java
+++ src/main/java/com/nextinno/doshare/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.nextinno.doshare.login;
 
+import java.util.Map;
+
 import javax.servlet.ServletException;
 
 import org.slf4j.Logger;
@@ -12,11 +14,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.nextinno.doshare.api.API;
 import com.nextinno.doshare.common.Common;
+import com.nextinno.doshare.domain.tokens.Token;
 import com.nextinno.doshare.domain.users.User;
+import com.nextinno.doshare.global.domain.GlobalDomain;
 import com.nextinno.doshare.login.mapper.LoginMapper;
 
 /**
@@ -26,42 +32,57 @@ import com.nextinno.doshare.login.mapper.LoginMapper;
 @RequestMapping(API.LOGIN)
 public class LoginController {
     private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
-    
+
     @Autowired
     private LoginMapper loginMapper;
-    
-    @RequestMapping(value = "signin", method = RequestMethod.POST, consumes="application/json")
+
+    @SuppressWarnings("rawtypes")
+    @RequestMapping(value = "signin", method = RequestMethod.POST, consumes = "application/json")
     @ResponseBody
-    public ResponseEntity<String> singin(@RequestBody final User reqUser)
-        throws ServletException {
+    // public ResponseEntity<GlobalDomain> singin(@RequestBody final User reqUser,
+    // @RequestParam(value= "remember", required = false) boolean remember) throws ServletException
+    // {
+    public ResponseEntity singin(@RequestBody Map<String, Object> params) throws ServletException {
+
+        GlobalDomain globalDomain = new GlobalDomain();
+        Token token = new Token();
         
         // to-do 암호화 하는 것 생각해 보기, 클라이언트에서 먼저 암호화 해서 보내고, 받고 해야 할 것 같다.
-        String password = reqUser.getPassword();
+
+        User reqUser = new User();
+        reqUser.setEmail((String) params.get("email"));
+        reqUser.setPassword((String) params.get("password"));
         
+        String password = reqUser.getPassword();
+
         // 1. DB에서 값을 가져온다.
         User user = certificationUser(reqUser);
-        if( user != null ){
-            
+        if (user != null) {
+
             // 2. password를 비교한다.
-            if(password.equals(user.getPassword())){
-                
+            if (password.equals(user.getPassword())) {
+
                 // 3. 아무런 변경이 없으면 token을 발행한다.
-                String token = Common.generateToken(user.getEmail(), user.getRole());
-                
-                return new ResponseEntity<String>(token, HttpStatus.OK);
+                token.setToken(Common.generateToken(user.getEmail(), user.getRole()));
+                // remember me를 누른 사람만 준다.
+                if((boolean) params.get("remember")) {
+                    token.setRefreshToken(Common.generateRefreshToken(user.getEmail(), user.getRole()));
+                }
+                return new ResponseEntity<Token>(token, HttpStatus.OK);
             } else {
-                
-                return new ResponseEntity<String>("Check your ID or Password.", HttpStatus.BAD_REQUEST);
+                globalDomain.setMessage("Check your ID or Password.");
+                return new ResponseEntity<GlobalDomain>(globalDomain, HttpStatus.BAD_REQUEST);
             }
         } else {
-            // 그리고 i18n을 적용하도록 한다. 
-            return new ResponseEntity<String>("Check your ID or Password.", HttpStatus.BAD_REQUEST);
+            // 그리고 i18n을 적용하도록 한다.
+            globalDomain.setMessage("Check your ID or Password.");
+            return new ResponseEntity<GlobalDomain>(globalDomain, HttpStatus.BAD_REQUEST);
         }
     }
-    
+
     // to-do 토큰 발행해 주는 것을 만든다.
-    // 먼저 해당 id와  증명된 사용자라면 다면 다시 토큰을 재 발행해 준다.
-    
+    // 먼저 해당 id와 증명된 사용자라면 다면 다시 토큰을 재 발행해 준다.
+
 
     /**
      * @param reqUser
@@ -71,11 +92,10 @@ public class LoginController {
     @SuppressWarnings("rawtypes")
     @RequestMapping(value = "signup", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseEntity signup(@RequestBody final User reqUser)
-        throws Exception {
-        
+    public ResponseEntity signup(@RequestBody final User reqUser) throws Exception {
+
         // 1. DB에 값이 있는지 확인한다.
-        if(!isExistUser(reqUser)){
+        if (!isExistUser(reqUser)) {
             // 2. 없으면 DB에 값을 insert 한다.
             loginMapper.addUser(reqUser);
         } else {
@@ -84,7 +104,7 @@ public class LoginController {
             logger.error("User already existed.");
             return new ResponseEntity<String>("User already existed.", HttpStatus.BAD_REQUEST);
         }
-        
+
         // 3. 201 ok를 준다.
         return new ResponseEntity(HttpStatus.CREATED);
     }
@@ -95,20 +115,20 @@ public class LoginController {
      */
     private User certificationUser(User reqUser) {
         User user = loginMapper.certificationUser(reqUser);
-        if( user != null ){
+        if (user != null) {
             return user;
         } else {
             return null;
         }
     }
-    
+
     /**
      * @param reqUser
      * @return
      */
     private boolean isExistUser(User reqUser) {
         User user = loginMapper.findByUser(reqUser);
-        if( user != null ){
+        if (user != null) {
             return true;
         } else {
             return false;
