e1349cc79f23573372828e8957ea9b4a3f6cd220
rsjung0320 <rsjung0320@gmail.com>
2016-10-17 16:37:51 +0900

    test

--- src/main/java/com/nextinno/doshare/domain/boards/Board.java
+++ src/main/java/com/nextinno/doshare/domain/boards/Board.java
@@ -7,6 +7,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.nextinno.doshare.domain.users.User;
 
 /**
  * @author rsjung
@@ -16,179 +19,35 @@ import javax.persistence.Id;
 @Entity
 public class Board implements Serializable{
     
+ // index
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
- 
-    @Column(name = "name", nullable = false)
-    private String name;
- 
-    @Column(name = "age", nullable = false)
-    private Integer age;
- 
-    public Long getId() {
-        return id;
-    }
- 
-    public void setId(Long id) {
-        this.id = id;
-    }
- 
-    public String getName() {
-        return name;
-    }
- 
-    public void setName(String name) {
-        this.name = name;
-    }
- 
-    public Integer getAge() {
-        return age;
-    }
- 
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-//    // index
-//    private int idx = 0;
-//    // 글 제목
-//    private String title = "";
-//    // 글쓴이
-//    private String email = "";
-//    // 글 쓴 날짜
-//    private String uploadDate = "";
-//    // 최종 수정
-//    private String updatedDate = "";
-//    // 조회 수
-//    private int readCount = 0;
-//    // 이미지 path
-//    private String imagePath = "";
-//    // 글 태그
-//    private String content = "";
-//    // user의 idx
-//    private int authorId = 0;
-//   
-//    
-//    /**
-//     * @return the idx
-//     */
-//    public int getIdx() {
-//        return idx;
-//    }
-//    /**
-//     * @param idx the idx to set
-//     */
-//    public void setIdx(int idx) {
-//        this.idx = idx;
-//    }
-//    /**
-//     * @return the title
-//     */
-//    public String getTitle() {
-//        return title;
-//    }
-//    /**
-//     * @param title the title to set
-//     */
-//    public void setTitle(String title) {
-//        this.title = title;
-//    }
-//    /**
-//     * @return the email
-//     */
-//    public String getEmail() {
-//        return email;
-//    }
-//    /**
-//     * @param email the email to set
-//     */
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//    /**
-//     * @return the uploadDate
-//     */
-//    public String getUploadDate() {
-//        return uploadDate;
-//    }
-//    /**
-//     * @param uploadDate the uploadDate to set
-//     */
-//    public void setUploadDate(String uploadDate) {
-//        this.uploadDate = uploadDate;
-//    }
-//    /**
-//     * @return the updatedDate
-//     */
-//    public String getUpdatedDate() {
-//        return updatedDate;
-//    }
-//    /**
-//     * @param updatedDate the updatedDate to set
-//     */
-//    public void setUpdatedDate(String updatedDate) {
-//        this.updatedDate = updatedDate;
-//    }
-//    /**
-//     * @return the readCount
-//     */
-//    public int getReadCount() {
-//        return readCount;
-//    }
-//    /**
-//     * @param readCount the readCount to set
-//     */
-//    public void setReadCount(int readCount) {
-//        this.readCount = readCount;
-//    }
-//    /**
-//     * @return the imagePath
-//     */
-//    public String getImagePath() {
-//        return imagePath;
-//    }
-//    /**
-//     * @param imagePath the imagePath to set
-//     */
-//    public void setImagePath(String imagePath) {
-//        this.imagePath = imagePath;
-//    }
-//    /**
-//     * @return the content
-//     */
-//    public String getContent() {
-//        return content;
-//    }
-//    /**
-//     * @param content the content to set
-//     */
-//    public void setContent(String content) {
-//        this.content = content;
-//    }
-//    /**
-//     * @return the authorId
-//     */
-//    public int getAuthorId() {
-//        return authorId;
-//    }
-//    /**
-//     * @param authorId the authorId to set
-//     */
-//    public void setAuthorId(int authorId) {
-//        this.authorId = authorId;
-//    }
-//    
-//    /* (non-Javadoc)
-//     * @see java.lang.Object#toString()
-//     */
-//    @Override
-//    public String toString() {
-//        StringBuilder builder = new StringBuilder();
-//        builder.append("Board [idx=").append(idx).append(", title=").append(title).append(", email=").append(email)
-//                .append(", uploadDate=").append(uploadDate).append(", updatedDate=").append(updatedDate)
-//                .append(", readCount=").append(readCount).append(", imagePath=").append(imagePath).append(", content=")
-//                .append(content).append(", authorId=").append(authorId).append("]");
-//        return builder.toString();
-//    }
+    @GeneratedValue
+    private long idx = 0;
+    // 글 제목
+    @Column(name = "title", nullable = false)
+    private String title = "";
+    // 글쓴이
+    @Column(name = "email", nullable = false)
+    private String email = "";
+    // 글 쓴 날짜
+    @Column(name = "upload_date", nullable = false)
+    private String uploadDate = "";
+    // 최종 수정
+    @Column(name = "updated_date")
+    private String updatedDate = "";
+    // 조회 수
+    @Column(name = "read_count")
+    private int readCount = 0;
+    // 이미지 path
+    @Column(name = "image_path")
+    private String imagePath = "";
+    // 글 태그
+    @Column(name = "content", nullable = false, columnDefinition = "mediumtext")
+    private String content = "";
+    // user의 idx
+    @ManyToOne
+    private User user;
+    
+    
 }
 
--- src/main/java/com/nextinno/doshare/domain/users/User.java
+++ src/main/java/com/nextinno/doshare/domain/users/User.java
@@ -1,85 +1,40 @@
 package com.nextinno.doshare.domain.users;
 
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+import com.nextinno.doshare.domain.boards.Board;
+
 /**
  * @author rsjung
  *
  */
+@Entity
 public class User {
-    private int idx = 0;
+    @Id
+    @GeneratedValue
+    private long idx;
+    
+    @Column(name = "email", nullable = false, unique=true)
     private String email = "";
+    
+    @Column(name = "name", nullable = false)
     private String name = "";   
+    
+    @Column(name = "password", nullable = false)
     private String password = "";
+    
     // user / admin
+    @Column(name = "role", nullable = false)
     private String role = "user";
     
-    /**
-     * @return the idx
-     */
-    public int getIdx() {
-        return idx;
-    }
-    /**
-     * @param idx the idx to set
-     */
-    public void setIdx(int idx) {
-        this.idx = idx;
-    }
-    /**
-     * @return the email
-     */
-    public String getEmail() {
-        return email;
-    }
-    /**
-     * @param email the email to set
-     */
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    /**
-     * @return the name
-     */
-    public String getName() {
-        return name;
-    }
-    /**
-     * @param name the name to set
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-    /**
-     * @return the password
-     */
-    public String getPassword() {
-        return password;
-    }
-    /**
-     * @param password the password to set
-     */
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    /**
-     * @return the role
-     */
-    public String getRole() {
-        return role;
-    }
-    /**
-     * @param role the role to set
-     */
-    public void setRole(String role) {
-        this.role = role;
-    }
-    /* (non-Javadoc)
-     * @see java.lang.Object#toString()
-     */
-    @Override
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-        builder.append("User [idx=").append(idx).append(", email=").append(email).append(", name=").append(name)
-                .append(", password=").append(password).append(", role=").append(role).append("]");
-        return builder.toString();
-    }
+    @OneToMany(targetEntity=Board.class)
+    @JoinColumn(nullable = true)
+    private List<Board> Boards;
 }
