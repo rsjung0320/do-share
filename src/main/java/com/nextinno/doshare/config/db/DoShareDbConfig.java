aeb0f1167ed24b4b7116391b7272e935ce7e9717
rsjung0320 <rsjung0320@gmail.com>
2016-10-17 13:01:04 +0900

    warning 제거

--- src/main/java/com/nextinno/doshare/config/db/DoShareDbConfig.java
+++ src/main/java/com/nextinno/doshare/config/db/DoShareDbConfig.java
@@ -7,7 +7,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -33,7 +32,8 @@ public class DoShareDbConfig {
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
         // to-do 설정 값에서 가져오도록 한다. github에 올라가면 안되는 정보이기 때문에
         // kpig7.synolog.me 3306
-        dataSource.setUrl("jdbc:mysql://14.63.216.137:3306/dodb?useUnicode=true&characterEncoding=utf8");
+//        dataSource.setUrl("jdbc:mysql://14.63.216.137:3306/dodb?useUnicode=true&characterEncoding=utf8");
+        dataSource.setUrl("jdbc:mysql://192.168.13.165:3306/dodb?useUnicode=true&characterEncoding=utf8");
         dataSource.setUsername("rsjung");
         dataSource.setPassword("rsjung");
 
